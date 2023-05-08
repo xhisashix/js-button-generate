65091441db52b70024d0e755819a8606378d5fa3
Hisashi Iwamoto <hisasi622@gmail.com>
2023-05-08 21:23:40 +0900

    option関係のボタンを追記

--- js/main.js
+++ js/main.js
@@ -5,47 +5,123 @@
 function getOption() {
   // フォームの値を取得
   var text = document.getElementById("text").value;
+  var backgroundColor = document.getElementById("background_color").value;
   var color = document.getElementById("color").value;
   var size = document.getElementById("size").value;
-  var link = document.getElementById("link").value;
-  var target = document.getElementById("target").value;
-  var border = document.getElementById("border").value;
-  var shadow = document.getElementById("shadow").value;
-  var radius = document.getElementById("radius").value;
-  var bold = document.getElementById("bold").value;
+  var borderWidth = document.getElementById("border_width").value;
+  var borderType = document.getElementById("border_type").value;
+  var borderColor = document.getElementById("border_color").value;
+  var shadow = document.getElementById("shadow").checked;
+  var radius = document.getElementById("radius").checked;
+  var bold = document.getElementById("bold").checked;
+  var italic = document.getElementById("italic").checked;
 
   // sizeの値によってボタンのサイズを変更
-  if (size === "small") {
-    var width = 100;
-    var height = 30;
-  } else if (size === "medium") {
-    var width = 150;
-    var height = 40;
-  } else if (size === "large") {
-    var width = 200;
-    var height = 50;
-  }else {
-    var width = 150;
-    var height = 40;
-  }
+  var sizeOption = setSize(size);
 
   // 配列に格納
   var optionArray = [];
   optionArray.text = text;
+  optionArray.backgroundColor = backgroundColor;
   optionArray.color = color;
-  optionArray.width = width;
-  optionArray.height = height;
-  optionArray.link = link;
-  optionArray.target = target;
-  optionArray.border = border;
-  optionArray.shadow = shadow;
-  optionArray.radius = radius;
-  optionArray.bold = bold;
+  optionArray.width = sizeOption.width;
+  optionArray.height = sizeOption.height;
+  optionArray.borderWidth = borderWidth;
+  optionArray.borderType = borderType;
+  optionArray.borderColor = borderColor;
+  optionArray.shadow = setShadow(shadow);
+  optionArray.radius = setRadius(radius);
+  optionArray.bold = setBold(bold);
+  optionArray.italic = setItalic(italic);
 
   return optionArray;
 }
 
-// ボタン生成の関数
+/**
+ * 横幅と高さの値を設定する関数
+ * @returns {array} sizeArray
+ */
+function setSize(size) {
+  var sizeArray = [];
+  if (size === "small") {
+    sizeArray.width = 100;
+    sizeArray.height = 30;
+  } else if (size === "medium") {
+    sizeArray.width = 150;
+    sizeArray.height = 40;
+  } else if (size === "large") {
+    sizeArray.width = 200;
+    sizeArray.height = 50;
+  } else {
+    sizeArray.width = 150;
+    sizeArray.height = 40;
+  }
+  return sizeArray;
+}
+
+/**
+ * 影を設定する関数
+ * @param {boolean} shadow
+ * @returns {string} shadowValue
+ */
+function setShadow(shadow) {
+  var shadowValue = "";
+  if (shadow === true) {
+    shadowValue = "3px 4px 4px rgba(0, 0, 0, 0.3)";
+  } else {
+    shadowValue = "none";
+  }
+  return shadowValue;
+}
+
+/**
+ * border-radiusを設定する関数
+ * @param {boolean} radius
+ * @returns {string} radiusValue
+ * */
+function setRadius(radius) {
+  var radiusValue = "";
+  if (radius === true) {
+    radiusValue = "5px";
+  } else {
+    radiusValue = "0px";
+  }
+  return radiusValue;
+}
+
+/**
+ * ふと文字を設定する関数
+ * @param {boolean} bold
+ * @returns {string} boldValue
+ * */
+function setBold(bold) {
+  var boldValue = "";
+  if (bold === true) {
+    boldValue = "bold";
+  } else {
+    boldValue = "normal";
+  }
+  return boldValue;
+}
+
+/**
+ * font-styleを設定する関数
+ * @param {boolean} italic
+ * @returns {string} italicValue
+ * */
+function setItalic(italic) {
+  var italicValue = "";
+  if (italic === true) {
+    italicValue = "italic";
+  } else {
+    italicValue = "normal";
+  }
+  return italicValue;
+}
+
+/**
+ * ボタン生成の関数
+ */
 function createButton() {
   var button = document.createElement("button");
 
@@ -65,13 +141,18 @@ function createButton() {
   if (option.text === "") {
     button.textContent = "ボタン";
   }
+
+  console.log(option.radius);
+  button.style.backgroundColor = option.backgroundColor;
   button.style.color = option.color;
   button.style.width = option.width + "px";
   button.style.height = option.height + "px";
-  button.style.border = option.border;
+  button.style.border =
+    option.borderWidth + "px " + option.borderType + " " + option.borderColor;
   button.style.boxShadow = option.shadow;
-  button.style.borderRadius = option.radius + "px";
+  button.style.borderRadius = option.radius;
   button.style.fontWeight = option.bold;
+  button.style.fontStyle = option.italic;
 
   // 複数のクラスを追加
   button.classList.add("button");
@@ -102,6 +183,7 @@ function getButtonCSS() {
   cssCodeArray.fontWeight = cssCode.fontWeight;
   cssCodeArray.borderRadius = cssCode.borderRadius;
   cssCodeArray.boxShadow = cssCode.boxShadow;
+  cssCodeArray.fontStyle = cssCode.fontStyle;
 
   // コード表示エリアにCSSコードを表示
   var codeArea = document.getElementById("code_area");
